f28a824ab7ac7995bab17e2a26f4e564466b1b7d
leminhnguyenai <leminhnguyenai@gmail.com>
2024-12-29 17:24:58 +0700

    Rename some variable in the method to generate the first population

--- genetic-aglorithm/utils.py
+++ genetic-aglorithm/utils.py
@@ -5,22 +5,22 @@ def generate_random_solutions(solution_length):
     for i in range(solution_length - 1):
         max += pow(2, i)
 
-    random_solutions = []
+    first_generation = []
 
-    for i in range(min, max):
-        random_solutions.append(generate_bin_as_str(i))
+    for i in range(min, max + 1):
+        first_generation.append(generate_bin_as_str(i))
 
     for i in range(solution_length - 1):
         single_pick_solution = generate_bin_as_str(pow(2, i))
         single_pick_solution = "0" * (solution_length - 1 - i) + single_pick_solution
 
-        random_solutions.append(single_pick_solution)
+        first_generation.append(single_pick_solution)
 
     blank_solution = "0" * solution_length
 
-    random_solutions.append(blank_solution)
+    first_generation.append(blank_solution)
 
-    return random_solutions
+    return first_generation
 
 
 def generate_bin_as_str(num):
